abd83fbd0d7617ec11b267187605c08cd3b79e60
천사관영 <janyouns@gmail.com>
2023-12-16 20:20:50 +0900

    [Fix] Player Movement
    Player Movement

--- HLK_2D_Project/Assets/1.Scene/LGY/3.Script/Player/Player_Move.cs
+++ HLK_2D_Project/Assets/1.Scene/LGY/3.Script/Player/Player_Move.cs
@@ -6,7 +6,8 @@ public class Player_Move : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rigidbody;
-    public float jumpForce = 5f;
+    public float jumpForce = 100f;
+    private bool isJumping = false;
 
     private void Awake()
     {
@@ -15,19 +16,29 @@ public class Player_Move : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(horizontalInput * moveSpeed, rigidbody.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
-            rigidbody.velocity = new Vector2(moveSpeed, rigidbody.velocity.y);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rigidbody.velocity = new Vector2( -moveSpeed,rigidbody.velocity.y);
-        }
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            //rigidbody.AddForce(Vector2.up * jumpForce , ForceMode2D.Impulse);
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isJumping = true;
         }
 
     }
+    private void FixedUpdate()
+    {
+        Debug.DrawRay(transform.position, Vector2.down, Color.red, 1f);
+        if (rigidbody.velocity.y < 0 )
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody.position, Vector2.down, 1, LayerMask.GetMask("Ground"));
+            if (hit.collider != null)
+            {
+                if (hit.distance < 0.5f)
+                {
+                    isJumping = false;
+                }
+            }
+        }
+    }
+
 }
