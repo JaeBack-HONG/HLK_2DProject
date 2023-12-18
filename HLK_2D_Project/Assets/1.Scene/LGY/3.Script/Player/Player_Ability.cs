ec721d898423d5472cf7e8977cf8aeabaaf037eb
Limkwan1004 <janyouns@gmail.com>
2023-12-18 12:04:59 +0900

    [Feat] Player_Ability_Add

---
+++ HLK_2D_Project/Assets/1.Scene/LGY/3.Script/Player/Player_Ability.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Ability : MonoBehaviour
+{
+    private bool isBrown = false;
+    private bool isKnight_1 = false;
+    private bool isHope_1 = false;
+    private bool isBlackWolf = false;
+}
