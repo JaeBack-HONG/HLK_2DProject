3f3c3e4e40a8d84db3751271eab8e9fcb9283c12
Limkwan1004 <janyouns@gmail.com>
2024-01-09 10:45:22 +0900

    [Feat] Add DiegoMod & ChompsMod Fix Value in inspector

--- HLK_2D_Project/Assets/1.Scene/LGY/3.Script/Player/Ability/Player_Diego_Mod.cs
+++ HLK_2D_Project/Assets/1.Scene/LGY/3.Script/Player/Ability/Player_Diego_Mod.cs
@@ -4,8 +4,45 @@ using UnityEngine;
 
 public class Player_Diego_Mod : Ability
 {
+    [SerializeField] private GameObject diego_Bullet_obj;
+    IEnumerator DiegoMod_co;
+
     public override void UseAbility()
     {
+        DiegoMod_co = DiegoAttack_co();
+        StartCoroutine(DiegoMod_co);
+    }
 
+    private IEnumerator DiegoAttack_co()
+    {
+        PlayerManager.instance.UsedAb();
+        P_state.actState = Unit_state.Default;
+        rigidbody.velocity = Vector2.zero;
+        animator.SetTrigger("DiegoMod");
+        animator.speed = anispeed * 1.5f;
+        yield return new WaitForSeconds(0.45f / anispeed);
+        CreateBullet();
+        noise.m_AmplitudeGain =2;
+        yield return new WaitForSeconds(0.16f / anispeed);
+        CreateBullet();
+        yield return new WaitForSeconds(0.16f / anispeed);
+        CreateBullet();
+        noise.m_AmplitudeGain =0;
+
+        yield return new WaitForSeconds(0.3f / anispeed);
+
+        animator.speed = 1;
+        animator.SetTrigger("Idle");
+        P_state.actState = Unit_state.Idle;
+
+        yield return null;
+    }
+
+    private void CreateBullet()
+    {
+        GameObject bullet = Instantiate(diego_Bullet_obj,
+            new Vector3(transform.position.x + P_state.direction.x, transform.position.y + 0.35f), Quaternion.identity);
+        Player_Projectile bullet_C = bullet.GetComponent<Player_Projectile>();
+        bullet_C.StartCoroutine(bullet_C.Shot(P_state.direction));
     }
 }
