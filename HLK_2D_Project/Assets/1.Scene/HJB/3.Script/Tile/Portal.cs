5813c748d6977213a9d5593b1d336cfdb4a1b74f
Hong Jae Baek <khks33@naver.com>
2024-01-10 15:53:23 +0900

    [Fix]Portal Bug

--- HLK_2D_Project/Assets/1.Scene/HJB/3.Script/Tile/Portal.cs
+++ HLK_2D_Project/Assets/1.Scene/HJB/3.Script/Tile/Portal.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Portal : MonoBehaviour
 {
+    IEnumerator waitSet_co;
+
+    private bool able = true;
+
     [SerializeField] GameObject portalArrowImage_obj;
     [Header("위치 설정")]
     [SerializeField] Vector2 teleportVectorSet;
+
+
+    private void coroutineSet(Collider2D col)
+    {
+        waitSet_co = WaitSet_Co(col);
+        StartCoroutine(waitSet_co);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -16,9 +28,14 @@ public class Portal : MonoBehaviour
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")&&Input.GetKey(KeyCode.UpArrow))
-        { 
-            collision.transform.position = teleportVectorSet;
+        if (collision.gameObject.CompareTag("Player")&&
+            Input.GetKey(KeyCode.UpArrow)&& able)
+            
+        {
+            able = false;
+            Debug.Log(able);
+            coroutineSet(collision);            
+            
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -28,6 +45,30 @@ public class Portal : MonoBehaviour
             portalArrowImage_obj.SetActive(false);
         }
     }
+
+    private IEnumerator WaitSet_Co(Collider2D col)
+    {
+
+        float currentTime = 0f;
+        while (currentTime < 0.1f)
+        {
+            currentTime += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        col.transform.position = teleportVectorSet;
+        currentTime = 0f;
+
+        while (currentTime<0.3f)
+        {
+            currentTime += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        able = true;        
+    }
+
+    
+
+    
 
 
 
