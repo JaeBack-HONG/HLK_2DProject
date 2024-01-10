80db24d0ba874ac94e3dfd5be874ee113f112f5a
Hong Jae Baek <khks33@naver.com>
2024-01-10 20:37:18 +0900

    [Feat]SaveDataSet

--- HLK_2D_Project/Assets/1.Scene/HJB/3.Script/DataSave/PlayerDataJson.cs
+++ HLK_2D_Project/Assets/1.Scene/HJB/3.Script/DataSave/PlayerDataJson.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 [System.Serializable]
 public class PlayerDataJson
-{   
-    
+{      
     //능력 종류 및 카운트 저장 변수 만들 것.
 
     public float maxHealth = 3f;
-    public float currentHealth = 3f;
+    public float currentHealth = 3f;    
     
-    public float Difficulty = 0;
     public string SceneName;    
 }
--- HLK_2D_Project/Assets/1.Scene/LGY/3.Script/ETC/GameManager.cs
+++ HLK_2D_Project/Assets/1.Scene/LGY/3.Script/ETC/GameManager.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.IO;
+using System;
+using Newtonsoft.Json;
+
 public enum Layer_Index
 {
     Ground =6,
@@ -19,18 +23,25 @@ public enum Animator_List
     BlackWolf,
 }
 
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
 
     [SerializeField] private GameObject OptionUI_obj;
-    
+    [SerializeField] private string introSceneName;
+    [SerializeField] private string currentSceneName;
+
+    public PlayerDataJson PlayerData;
+
+    public Player_State player;
 
     private void Awake()
     {        
-        if (instance == null )
+     
+        if (instance == null)
         {
-            instance = this;
+            instance = this;            
             DontDestroyOnLoad(gameObject);
         }
         else
@@ -38,14 +49,33 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+    }
 
+    
+    private void Start()
+    {
+        //씬이동후 현재 씬 저장
+        currentSceneName = SceneManager.GetActiveScene().name;
+        if (!currentSceneName.Equals(introSceneName))
+        {
+            DataSave();
+        }
     }
 
     private void Update()
     {
         OptionKeyDown_Set();
+        GetCompoPlayerCheck();
     }
-
+    private void OnApplicationQuit()
+    {
+        //게임 종료일 때 저장
+        currentSceneName = SceneManager.GetActiveScene().name;
+        if (!currentSceneName.Equals(introSceneName))
+        {
+            DataSave();
+        }
+    }
     private void OptionKeyDown_Set()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -56,23 +86,93 @@ public class GameManager : MonoBehaviour
         }
     }
 
+
+    //기본 셋팅
+    private void InitSetting()
+    {
+        PlayerData.maxHealth = 3f;
+        PlayerData.currentHealth = 3f;
+        PlayerData.SceneName = "";
+    }
+    private void GetCompoPlayerCheck()
+    {        
+        try
+        {
+            if (SceneManager.GetActiveScene().name != introSceneName && player == null)
+            {
+                player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_State>();
+            }
+
+        }
+        catch (NullReferenceException e)
+        {
+            player = null;
+        }
+    }
+    public void DataSave()
+    {
+        PlayerData.maxHealth = 0;
+        PlayerData.currentHealth = player.Health;
+        PlayerData.SceneName = currentSceneName;
+
+        string fileName;
+
+        fileName = Application.dataPath + "PlayerDataJosn.json";
+        if (!File.Exists(fileName))
+        {
+            File.Create(fileName);
+        }
+        string toJson = JsonConvert.SerializeObject(PlayerData, Formatting.Indented);
+
+        File.WriteAllText(fileName, toJson);
+
+    }
+    public PlayerDataJson DataLoad()
+    {
+        string fileName;
+        try
+        {
+            fileName = Application.dataPath + "/PlayerDataJson.json";
+            string readData = File.ReadAllText(fileName);
+            PlayerDataJson playerData = new PlayerDataJson();
+            PlayerData = JsonConvert.DeserializeObject<PlayerDataJson>(readData);
+            PlayerData = playerData;
+            return playerData;
+        }
+        catch (DirectoryNotFoundException e)
+        {
+            Debug.Log(e);
+            return null;
+            
+        }
+    }
+    public void DeletSaveData()
+    {
+        string fileName;
+        fileName = Application.dataPath + "/PlayerDataJson.json";
+        File.Delete(fileName);
+    }
+
+
+    #region //버튼 이벤트 메서드
     public void MainGame_1()
     {
-        SceneManager.LoadScene("HJB_Scene");        
+        SceneManager.LoadScene("HJB_Scene");
+        GetCompoPlayerCheck();
     }
     public void MainMenu_Btn()
     {           
         OptionUI_obj.SetActive(!OptionUI_obj.activeSelf);
         Time.timeScale = OptionUI_obj.activeSelf ? 0 : 1;
         SceneManager.LoadScene("MainMenu");        
-    }
-        
+    }        
 
     public void OnClickOption_Btn()
     {
         OptionUI_obj.SetActive(!OptionUI_obj.activeSelf);
         Time.timeScale = OptionUI_obj.activeSelf ? 0 : 1;
     }
+    #endregion
     public void ExitGame()
     {
 #if UNITY_EDITOR
