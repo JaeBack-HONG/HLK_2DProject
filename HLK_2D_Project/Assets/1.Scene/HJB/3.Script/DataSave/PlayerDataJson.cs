b9d667ab1ad68ba6407715a45e2ca2b752c90f64
Hong Jae Baek <khks33@naver.com>
2024-01-10 17:43:21 +0900

    [Fix]Roach Scale Fix

---
+++ HLK_2D_Project/Assets/1.Scene/HJB/3.Script/DataSave/PlayerDataJson.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerDataJson
+{   
+    
+    //능력 종류 및 카운트 저장 변수 만들 것.
+
+    public float maxHealth = 3f;
+    public float currentHealth = 3f;
+    
+    public float Difficulty = 0;
+    public string SceneName;    
+}
