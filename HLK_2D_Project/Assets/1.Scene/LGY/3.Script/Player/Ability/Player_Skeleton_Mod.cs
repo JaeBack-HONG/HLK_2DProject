112751a2464596abe285981809633410b8f823df
Limkwan1004 <janyouns@gmail.com>
2024-01-11 15:17:46 +0900

    [Fix] Sort Ability Array & IconName

---
+++ HLK_2D_Project/Assets/1.Scene/LGY/3.Script/Player/Ability/Player_Skeleton_Mod.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Skeleton_Mod : Ability
+{
+    public override void UseAbility()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
