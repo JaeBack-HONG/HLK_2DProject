6f819f6f2025c94081cb6d2705423a8f993f63aa
Limkwan1004 <janyouns@gmail.com>
2024-01-11 15:30:14 +0900

    [Fix] BrownMod Animation

--- HLK_2D_Project/Assets/1.Scene/LGY/3.Script/Player/Ability/Player_Brown_Mod.cs
+++ HLK_2D_Project/Assets/1.Scene/LGY/3.Script/Player/Ability/Player_Brown_Mod.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Brown_Mod : Ability
@@ -13,16 +12,15 @@ public class Player_Brown_Mod : Ability
 
     private void Brown_Ability()
     {
-        P_state.isAttack = true;
-        float ho = Input.GetAxis("Horizontal");
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, ho * Vector2.right, 3f, LayerMask.GetMask("Enemy"));
-        Debug.DrawRay(transform.position, Vector2.right * 3f, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, P_state.direction, 3f, LayerMask.GetMask("Enemy"));
+
         if (hit.collider != null)
         {
             P_state.actState = Unit_state.Default;
             PlayerManager.instance.UsedAb();
             hit.collider.transform.position = new Vector2(transform.position.x, transform.position.y + 3f);
             animator.SetTrigger("BrownMod");
+            animator.speed = anispeed;
             hit.collider.TryGetComponent<Monster_State>(out Monster_State M_state);
             hit.collider.TryGetComponent<Rigidbody2D>(out Rigidbody2D otherRigid);
             gravityscale = otherRigid.gravityScale;
@@ -36,21 +34,21 @@ public class Player_Brown_Mod : Ability
         }
     }
 
-
     IEnumerator Brown_Ab(Rigidbody2D otherRigid, Monster_State M_state)
     {
-        
+
         rigidbody.velocity = Vector2.zero;
 
 
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.2f / anispeed);
         otherRigid.gravityScale = gravityscale;
         M_state.state = Unit_state.Default;
         otherRigid.AddRelativeForce(-P_state.direction * 20f, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.4f / anispeed);
         animator.SetTrigger("Idle");
-        yield return new WaitForSeconds(0.2f);
+        animator.speed = 1f;
         P_state.actState = Unit_state.Idle;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1.1f);
         M_state.state = Unit_state.Move;
         P_state.isAttack = false;
         yield return null;
