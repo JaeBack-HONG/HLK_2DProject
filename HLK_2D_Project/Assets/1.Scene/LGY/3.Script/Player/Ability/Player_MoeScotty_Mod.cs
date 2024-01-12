ae10dbcd500fef444b879eb3f7e1a36f7a0a36cf
Limkwan1004 <janyouns@gmail.com>
2024-01-12 10:48:34 +0900

    [Feat] MoeMod

--- HLK_2D_Project/Assets/1.Scene/LGY/3.Script/Player/Ability/Player_MoeScotty_Mod.cs
+++ HLK_2D_Project/Assets/1.Scene/LGY/3.Script/Player/Ability/Player_MoeScotty_Mod.cs
@@ -4,19 +4,48 @@ using UnityEngine;
 
 public class Player_MoeScotty_Mod : Ability
 {
+    [SerializeField] float glideSpeed = 5f;
+    [SerializeField] int dmg = 1;
+
     public override void UseAbility()
     {
+        StartCoroutine(MoeAttack());
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private IEnumerator MoeAttack()
     {
+        gameObject.layer = (int)Layer_Index.Hit;
+        rigidbody.velocity = Vector2.zero;
+        P_state.actState = Unit_state.Idle;
+        animator.SetTrigger("MoeScotty");
+        Input.ResetInputAxes();
+        PlayerManager.instance.UsedAb();
+        while (P_state.JumState.Equals(Jump_State.Jumping) || P_state.JumState.Equals(Jump_State.Falling))
+        {
+            rigidbody.velocity = new Vector2(P_state.direction.x * glideSpeed, -0.5f);
+            if(Input.GetKeyDown(KeyCode.Z))
+            {
+                break;
+            }
+            yield return null;
+        }
         
+        gameObject.layer = (int)Layer_Index.Player;
+        animator.SetTrigger("Idle");
+        yield return null;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.layer.Equals((int)Layer_Index.Enemy))
+        {
+            rigidbody.velocity = Vector2.zero;
+            Monster_State monstate = collision.gameObject.GetComponent<Monster_State>();
+            monstate.Health -= dmg; 
+            monstate.UnitHit.Hit(monstate.gameObject.layer, transform.position);
+            P_state.Health += dmg;
+            P_state.Health = P_state.Health > P_state.data.HP ? P_state.data.HP : P_state.Health;
+        }
     }
+
 }
